7f85024dbc22f8bf503623275f83a9cb89b0286c
xiaohh <1296522554@qq.com>
2021-10-27 14:45:04 +0800

    创建Home、NotebookList、NoteDetail、TashDetail组件，并配置好对应的路由

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+   <div id="login">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+  name: "Login",
+  data(){
+    return {
+      msg:"This id login page"
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+ h1 {
+   color:skyblue;
+ }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../components/Home.vue'
+import Login from "../components/Login";
+import NotebookList from "@/components/NotebookList";
+import NoteDetail from "@/components/NoteDetail";
+import TashDetail from "@/components/TashDetail";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path:'/login',
+    component: Login
+  },
+  {
+    path:"/notebooks",
+    component: NotebookList
+  },
+  {
+    path:"/note/:noteId",
+    component: NoteDetail
+  },
+  {
+    path:"/trash/:noteId",
+    component: TashDetail
   }
 ]
 
